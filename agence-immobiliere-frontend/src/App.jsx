3158c5aeecfd426e3ef49d67b2ea46a908ea480a
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-11-24 23:44:23 +0100

    Ajout du footer

--- agence-immobiliere-frontend/src/App.jsx
+++ agence-immobiliere-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Immeubles from './pages/Immeubles';
 import Appartements from './pages/Appartements';
 import Contact from './pages/Contact';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/appartements" element={<Appartements />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
