dbeee3474055748a1b94ac1674591366442784ad
Elhadj Tall <elhadjoumartall10@gmail.com>
2024-11-24 23:50:53 +0100

    AJout du dashboard

--- agence-immobiliere-frontend/src/App.jsx
+++ agence-immobiliere-frontend/src/App.jsx
@@ -5,18 +5,25 @@ import Immeubles from './pages/Immeubles';
 import Appartements from './pages/Appartements';
 import Contact from './pages/Contact';
 import Footer from './components/layout/Footer';
+import Dashboard from './pages/admin/Dashboard';
 
 function App() {
   return (
     <Router>
       <div>
+        {/* Navigation bar c'est à dire le menu de navigation notre application */}
         <Navigation />
+
+        {/* Contenu de notre application en fonction de l'url et les routes et les pages de notre site */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/immeubles" element={<Immeubles />} />
           <Route path="/appartements" element={<Appartements />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/admin/dashboard" element={<Dashboard/>} />
         </Routes>
+
+        {/* Pied de page le footer */}
         <Footer />
       </div>
     </Router>
